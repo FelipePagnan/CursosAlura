f128f2076df4e52dbc4d1b1b59e7942af6f03eeb
Felipe Pagnan <luizfelipe130@outlook.com>
2024-12-25 00:41:32 -0300

    Segunda parte em 18%

---
+++ Aprenda a programar em C# com Orientação a Objetos/2_OrientacaoAObjetos/ConsoleApp1/ConsoleApp1/Musica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel {  get; set; }
+
+    public string NomeCompleto { get; set; }
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome {Nome}");
+        Console.WriteLine($"Artista {Artista}");
+        Console.WriteLine($"Duracao {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
---
+++ Aprenda a programar em C# com Orientação a Objetos/2_OrientacaoAObjetos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Musica musica1 = new Musica();
+        musica1.Nome = "Roxane";
+        musica1.Artista = "The Police";
+        musica1.Duracao = 273;
+        musica1.Disponivel = true;
+        Console.WriteLine(musica1.Disponivel);
+
+        Musica musica2 = new Musica();
+        musica2.Nome = "Vertigo";
+        musica2.Artista = "U2";
+        musica2.Duracao = 267; 
+        musica2.Disponivel = false;
+
+        musica1.ExibirFichaTecnica();
+        musica2.ExibirFichaTecnica();
+
+        Console.ReadLine();
+    }
+}
